374f7729cf0c077b729ca49f03b355fde2e769cd
Oleg Efimenko <50793224+Navirator@users.noreply.github.com>
2023-03-22 09:11:53 +0200

    22.03.2023 Home page appdate

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -9,4 +9,48 @@ class Home extends Component {
     }
 }
 
+//22.03.2023 oleg
+            
+<Container>
+    <h2 className="textcenter m-4">Our team</h2>
+    <div className="row">
+        <div className="col">
+            <Card className="m-4 text-center" bg="light" border="primary">
+                <Card.img
+                variant="top"
+                src="https://qwertify.io/wp-content/uploads/2201/04/Team.jpeg"/>
+                <Card.Body>
+                    <Card.Title>Developers</Card.Title>
+                    <Card.text>Команда 1</Card.text>
+                    <Button variant="primary">About team</Button>
+                </Card.Body>
+            </Card>
+        </div>
+        <div className="col">
+            <Card className="m-4 text-center" bg="light" border="primary">
+                <Card.img
+                variant="top"
+                src="https://cdn.vox-cdn.com/thumor/Ndb49Uk3hjiquS041NDD0tPDPAs=/0x169:1423x914/fit-in/1200x630/cdn.vox-"/>
+                <Card.Body>
+                    <Card.Title>Природознавці</Card.Title>
+                    <Card.text>Команда 2</Card.text>
+                    <Button variant="primary">About team</Button>
+                </Card.Body>
+            </Card>
+        </div>
+        <div className="col">
+            <Card className="m-4 text-center" bg="light" border="primary">
+                <Card.img
+                variant="top"
+                src="https://qwertify.io/wp-content/uploads/2201/04/Team.jpeg"/>
+                <Card.Body>
+                    <Card.Title>Developers</Card.Title>
+                    <Card.text>Команда 1</Card.text>
+                    <Button variant="primary">About team</Button>
+                </Card.Body>
+            </Card>
+        </div>
+    </div>                
+</Container>
+
 export default Home;
