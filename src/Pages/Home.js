2965cd80b284f54551e86a56022157b9e2ae11ad
Oleg Efimenko <50793224+Navirator@users.noreply.github.com>
2023-03-28 19:00:37 +0300

    Edit Home.js

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,56 +1,60 @@
 import React, {Component} from 'react';
 import CarouselBoxHk from "../Components/CarouselBoxHk"
+import {Button, Card, CardImg, Container} from "react-bootstrap";
 
 class Home extends Component {
     render() {
         return (
+            <>
             <CarouselBoxHk/>
+            
+{/* 22.03.2023 oleg */}
+            
+            <Container>
+                <h2 className="textcenter m-4">Our team</h2>
+                <div className="row">
+                    <div className="col">
+                        <Card className="m-4 text-center" bg="light" border="primary">
+                            <Card.Img
+                            variant="top"
+                            src="https://www.cyberark.com/wp-content/uploads/2019/11/Developer.jpg"/>
+                            <Card.Body>
+                                <Card.Title>Developers</Card.Title>
+                                <Card.Text>Команда 1</Card.Text>
+                                <Button variant="primary">About team</Button>
+                            </Card.Body>
+                        </Card>
+                    </div>
+                    <div className="col">
+                        <Card className="m-4 text-center" bg="light" border="primary">
+                            <Card.Img
+                            variant="top"
+                            src="https://cdn.vox-cdn.com/thumbor/Ndb49Uk3hjiquS041NDD0tPDPAs=/0x169:1423x914/fit-in/1200x630/cdn.vox-cdn.com/uploads/chorus_asset/file/7342855/microsoftteams.0.jpg"/>
+                            <Card.Body>
+                                <Card.Title>Природознавці</Card.Title>
+                                <Card.Text>Команда 2</Card.Text>
+                                <Button variant="primary">About team</Button>
+                            </Card.Body>
+                        </Card>
+                    </div>
+                    <div className="col">
+                        <Card className="m-4 text-center" bg="light" border="primary">
+                            <Card.Img
+                            variant="top"
+                            src="https://www.kv.by/sites/default/files/pictures/userpictures/2019/11/29/2359/foto6_1.jpg"/>
+                            <Card.Body>
+                                <Card.Title>Маркетилоги</Card.Title>
+                                <Card.Text>Команда 3</Card.Text>
+                                <Button variant="primary">About team</Button>
+                            </Card.Body>
+                        </Card>
+                    </div>
+                </div>                
+            </Container>
+            </>
+            
         );
     }
 }
-
-//22.03.2023 oleg
-            
-<Container>
-    <h2 className="textcenter m-4">Our team</h2>
-    <div className="row">
-        <div className="col">
-            <Card className="m-4 text-center" bg="light" border="primary">
-                <Card.img
-                variant="top"
-                src="https://qwertify.io/wp-content/uploads/2201/04/Team.jpeg"/>
-                <Card.Body>
-                    <Card.Title>Developers</Card.Title>
-                    <Card.text>Команда 1</Card.text>
-                    <Button variant="primary">About team</Button>
-                </Card.Body>
-            </Card>
-        </div>
-        <div className="col">
-            <Card className="m-4 text-center" bg="light" border="primary">
-                <Card.img
-                variant="top"
-                src="https://cdn.vox-cdn.com/thumor/Ndb49Uk3hjiquS041NDD0tPDPAs=/0x169:1423x914/fit-in/1200x630/cdn.vox-"/>
-                <Card.Body>
-                    <Card.Title>Природознавці</Card.Title>
-                    <Card.text>Команда 2</Card.text>
-                    <Button variant="primary">About team</Button>
-                </Card.Body>
-            </Card>
-        </div>
-        <div className="col">
-            <Card className="m-4 text-center" bg="light" border="primary">
-                <Card.img
-                variant="top"
-                src="https://qwertify.io/wp-content/uploads/2201/04/Team.jpeg"/>
-                <Card.Body>
-                    <Card.Title>Developers</Card.Title>
-                    <Card.text>Команда 1</Card.text>
-                    <Button variant="primary">About team</Button>
-                </Card.Body>
-            </Card>
-        </div>
-    </div>                
-</Container>
 
 export default Home;
