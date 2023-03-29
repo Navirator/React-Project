02b59aba9d41b18f5efc0bcc9569c7e51d444a77
Oleg Efimenko <50793224+Navirator@users.noreply.github.com>
2023-03-29 09:02:36 +0300

    Edit Header.js 29.03.2023

--- src/Header.js
+++ src/Header.js
@@ -1,11 +1,12 @@
 import React, { Component } from "react";
 import {
-    Navbar,
-    Nav,
-    FormControl,
-    Container,
-    Form,
-    Button
+    Navbar, 
+    Nav, 
+    FormControl, 
+    Container, 
+    Form, 
+    Button, 
+    Modal /* 29.03.2023 */
 } from "react-bootstrap";
 
 import logo from "./assets/Logo.jpg";
@@ -21,12 +22,45 @@ export default class Header extends Component {
     render() {
         return (
             <>
+
+            {/* 29.03.2023 */}
+
+            <Modal show={show} onHide={handleClose}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Log in</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <Form>
+                        <Form.Group controlId="fromBasicEmail">
+                            <Form.Label>Email Address</Form.Label>
+                            <Form.Control type="email" placeholder="Enter email" />
+                            <Form.Text className="text-muted"> We'll never share your email with anyone else. </Form.Text>
+                        </Form.Group>
+
+                        <Form.Group controlId="fromBasicPassword">
+                            <Form.Label>Password</Form.Label>
+                            <Form.Control type="password" placeholder="Enter password"> </Form.Control>
+                        </Form.Group>
+
+                        <Form.Group controlId="fromBasicCheckbox">
+                            <Form.Check type="checkbox" label="Remember me"/>
+                        </Form.Group>
+
+                        <Button variant="primary" type="submit"> Submit </Button>
+                    </Form>
+                </Modal.Body>
+            </Modal>
+
+
             <Router> 
                 <Routes> 
                     <Route path="/" element={<Home />}/>
                     <Route path="/about" element={<About/>}/>
                     <Route path="/contacts" element={<Contacts/>}/>
                     <Route path="/blog" element={<Blog/>}/>
+                    
+                    <Button className="ms-2" onClick={handleShow}>Login</Button> {/* 29.03.2023 */}
+
                 </Routes>
             </Router>
             <Navbar fixed="top" collapseOnSelect expand="md" bg="dark" variant="dark">
@@ -56,4 +90,12 @@ export default class Header extends Component {
             </>
         )
     }
+}
+
+{/* 29.03.2023 */}
+
+export default function Header() {
+    const [show, setShow] = useState(false);
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
 }
