d24af9b3b020b691386cb09f4cd551af22251579
Oleg Efimenko <50793224+Navirator@users.noreply.github.com>
2023-05-09 22:17:13 +0300

    Add files via upload

--- src/Pages/Blog.js
+++ src/Pages/Blog.js
@@ -1,81 +1,98 @@
-import React, {Component} from 'react';
-import {Container, Col, Row, Card, ListGroup} from "react-bootstrap";
+import React, { Component, useState } from 'react';
+import blogPosts from '../blogPosts';
+import { Row, Col, ListGroup, Button, Card, Container, Pagination } from 'react-bootstrap';
 
-class Blog extends Component {
-    render() {
-        return (
-            <Row>
-                <h1 className="textcenter m-4"></h1>
-                <Col md="9">
-                    
-                    <div className="d-flex align-items-center me-5">
-                        <div className="flex-shrink-0">
-                            <img 
-                                width={150} 
-                                height={150} 
-                                className="mr-3" 
-                                src="https://emgotas.files.wordpress.com/2016/11/what-is-a-team.jpg" 
-                                alt="photo" />
-                        </div>
-                        <div className="flex-grow-1 ms-3">
-                            <h5><a href='/blog/blog1'>Blog post 1</a></h5>
-                            <p> Lorem </p>
-                        </div>
-                    </div>
+export default function Blog() {
+    var postItems = blogPosts();
+    const [sortState, setSortState] = useState("none");
 
-                    <div className="d-flex align-items-center me-5">
-                        <div className="flex-shrink-0">
-                            <img 
-                                width={150} 
-                                height={150} 
-                                className="mr-3" 
-                                src="https://emgotas.files.wordpress.com/2016/11/what-is-a-team.jpg" 
-                                alt="photo" />
-                        </div>
-                        <div className="flex-grow-1 ms-3">
-                            <h5><a href='/blog/blog2'>Blog post 2</a></h5>
-                            <p> Lorem </p>
-                        </div>
-                    </div>
+    const sortMethods = {
+        none: { method: (a, b) => null },
+        ascending: { method: (a, b) => a.date > b.date ? 1 : -1 },
+        descending: { method: (a, b) => a.date < b.date ? 1 : -1 }
+    };
 
-                    <div className="d-flex align-items-center me-5">
-                        <div className="flex-shrink-0">
-                            <img 
-                                width={150} 
-                                height={150} 
-                                className="mr-3" 
-                                src="https://emgotas.files.wordpress.com/2016/11/what-is-a-team.jpg" 
-                                alt="photo" />
-                        </div>
-                        <div className="flex-grow-1 ms-3">
-                            <h5><a href='/blog/blog3'>Blog post 3</a></h5>
-                            <p> Lorem </p>
-                        </div>
-                    </div>
-                </Col>
-                <Col md="3">
-                    <h5 className="text-center mt-5">Категорії</h5>
-                    <Card>
-                        <ListGroup variant="flush">
-                            <ListGroup.Item>категорія 1</ListGroup.Item>
-                            <ListGroup.Item>категорія 2</ListGroup.Item>
-                            <ListGroup.Item>категорія 3</ListGroup.Item>
-                            <ListGroup.Item>категорія 4</ListGroup.Item>
-                            <ListGroup.Item>категорія 5</ListGroup.Item>
-                        </ListGroup>
-                    </Card>
-
-                    <Card className="mt-3 bg-light">
-                        <Card.Body>
-                            <Card.Title>Slide widget</Card.Title>
-                            <Card.Text> Lorem </Card.Text>
-                        </Card.Body>
-                    </Card>
-                </Col>
-            </Row>
-            //<div> Blog </div>
-        );
-    }
+    return (
+        <>
+            <Container className="mt-5">
+                <Row>
+                    <Col md="9" className='mt-5'>
+                        <Container>
+                            <Row>
+                                <Col md='12' className='ms-3'>
+                                    <Button onClick={(e) => setSortState("ascending")} className='btn btn-secondary me-3'>Date ↑</Button>
+                                    <Button onClick={(e) => setSortState("descending")} className='btn btn-secondary'>Date ↓</Button>
+                                </Col>
+                            </Row>
+                            <Row>
+                                <Col md='12'>
+                                    {
+                                        postItems.sort(sortMethods[sortState].method).map((item) => (
+                                            <div className='d-flex align-item-center me-5 border-1 border-bottom mb-2 p-3'>
+                                                <div className='flex-shrink-0'>
+                                                    <img src={item.imgUrl} alt='post img' className='mr-3' width={150} height={150} />
+                                                </div>
+                                                <div className='flex-grow-1 ms-3'>
+                                                    <h5>{item.name}</h5>
+                                                    <p>{item.date.toLocaleDateString()}</p>
+                                                    <p>{item.info}</p>
+                                                </div>
+                                            </div>
+                                        ))
+                                    }
+                                </Col>
+                            </Row>
+                            {/* <div> 
+                                <ul className="pagination">
+                                    <li className="page-item">1</li>
+                                    <li className="page-item">2</li>
+                                    <li className="page-item">3</li>
+                                    <li className="page-item">4</li>
+                                    <li className="page-item">5</li>
+                                    <li className="page-item">6</li>
+                                    <li className="page-item">7</li>
+                                    <li className="page-item">8</li>
+                                    <li className="page-item">9</li>
+                                    <li className="page-item">10</li>
+                                </ul>
+                                <Pagination
+                                    activePage={this.state.activePage}
+                                    itemsCountPerPage={2}
+                                    totalItemsCount={10}
+                                    pageRangeDisplayed={5}
+                                    onChange={this.handlePageChange.bind(this)}
+                                    itemClass="page-item"
+                                    linkClass="page-link"
+                                />
+                            </div> */}
+                        </Container>
+                    </Col>
+                    <Col md="3" className='mt-5'>
+                        <h5 className="text-center-md-5">Категорії</h5>
+                        <Card>
+                            <ListGroup variant="flush">
+                                <ListGroup.Item>RPG</ListGroup.Item>
+                                <ListGroup.Item>Racings</ListGroup.Item>
+                                <ListGroup.Item>FPS</ListGroup.Item>
+                                <ListGroup.Item>Strategy</ListGroup.Item>
+                                <ListGroup.Item>Simulator</ListGroup.Item>
+                            </ListGroup>
+                        </Card>
+                        <Card className="mt-3 bg-light">
+                            <Card.Body>
+                                <Card.Title>Slide Widget</Card.Title>
+                                <Card.Text>
+                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus debitis, id
+                                    corrupti quasi ipsa blanditiis aut itaque natus explicabo. Dolores repudiandae
+                                    minima, maiores nam aliquid consequatur doloremque magnam quidem nemo laborum est
+                                    quia sed blanditiis necessitatibus iure nihil commodi officia saepe temporibus et
+                                    dolor libero fuga, aut illum. Eos, odio.
+                                </Card.Text>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                </Row>
+            </Container>
+        </>
+    );
 }
-
-export default Blog;
